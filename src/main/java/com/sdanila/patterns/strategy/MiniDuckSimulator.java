a74644a1f1209ea958d6fd02e130954abba9e843
Stefan Danila <sdanila@axway.com>
2023-12-08 17:13:38 +0200

    patterns - strategy

--- src/main/java/com/sdanila/patterns/strategy/MiniDuckSimulator.java
+++ src/main/java/com/sdanila/patterns/strategy/MiniDuckSimulator.java
@@ -2,6 +2,10 @@ package com.sdanila.patterns.strategy;
 
 import com.sdanila.patterns.strategy.behavior.FlyRocketPowered;
 
+//The Strategy Pattern defines a family of algorithms,
+//encapsulates each one, and makes them interchangeable.
+//Strategy lets the algorithm vary independently of
+//clients that use it.
 public class MiniDuckSimulator {
     public static void main(String[] args) {
         System.out.println("Mallard duck");
